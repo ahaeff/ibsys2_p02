cdcc57e71f1551a61257c2abb3ecd38a4f8994b7
haeff <haeff@SKYNET03>
2012-12-29 12:16:02 +0100

    new functions / documentation added

--- scstool/src/scstool/obj/ProductionProg.java
+++ scstool/src/scstool/obj/ProductionProg.java
@@ -1,21 +1,42 @@
 package scstool.obj;
 
+/**
+ * @author haeff
+ *
+ * Stellt den Produktionswunsch und Prognose pro Produkt dar
+ *
+ */
 public class ProductionProg 
 {
+	//Produkt 1,2 oder 3
+	private int p;
+	
+	// Absatz + Prognose Zahlen in den Perioden n,n+1,n+2...
 	private int n;
 	private int n1;
 	private int n2;
 	private int n3;
 		
-	public ProductionProg(int n, int n1, int n2, int n3) 
+	public ProductionProg(int p, int n, int n1, int n2, int n3) 
 	{
 		super();
+		this.p = p;
 		this.n = n;
 		this.n1 = n1;
 		this.n2 = n2;
 		this.n3 = n3;
 	}
 	
+	public int getP() 
+	{
+		return p;
+	}
+	
+	public void setP(int p) 
+	{
+		this.n = p;
+	}
+		
 	public int getN() 
 	{
 		return n;
