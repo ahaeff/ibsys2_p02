067b3c29bf4c20f0634da124d51eb212defbdc7c
Viktor <fritzviktor@gmail.com>
2012-12-29 17:42:18 +0100

    Bedarfsmatrix hinzugefügt Durschnittsbedarf hinzugefügt Kaufteile hinzugefügt

--- scstool/src/scstool/proc/OrderService.java
+++ scstool/src/scstool/proc/OrderService.java
@@ -11,14 +11,20 @@ import scstool.obj.Order;
 /**
  * @author Alexander
  *
+ *Überlegungen :
+ *Risiko einbinden
+ *Listen haben keine Ids! Hoffen dass sie in der richtigen Reihenfolge sind?!
+ *
+ *
+ *
  */
 public class OrderService {
-	
+	public DatabaseContentHandler dbch = DatabaseContentHandler.get();
 	/**
 	 * Liste der (K) Material Objekte
 	 */
-	private List<Material> listOfMaterial = new ArrayList<Material>();
-	//TODO Zuordnung der K-Teile
+	private List<Material> listOfMaterial = dbch.getKTeile();
+	
 	
 	/**
 	 * Liste der eingehenden und offenen Bestellungen
@@ -29,23 +35,39 @@ public class OrderService {
 	/**
 	 * Liste des Bedarfs (Matrix-Ergebnisse)
 	 */
-	//TODO Bedarfsliste fehlt
+	private int[][] needs = MatrixMultiplication();
 	
 	/**
 	 * Durchschnitt des Periodenbedarfs
 	 */
-	//TODO Bedarfsliste nötig
+	private List<Double> averageNeeds = calculateAverageNeeds();
 	
 	/**
 	 * Reichweitensicherung
 	 */
 	//TODO Bedarfsliste nötig
 	
+	public List<Double> getAverageNeeds() {
+		return averageNeeds;
+	}
+
+	public void setAverageNeeds(List<Double> averageNeeds) {
+		this.averageNeeds = averageNeeds;
+	}
+
 	/**
 	 * Neubestellung notwendig
 	 */
 	private boolean order;
 	
+	public int[][] getNeeds() {
+		return needs;
+	}
+
+	public void setNeeds(int[][] needs) {
+		this.needs = needs;
+	}
+
 	/**
 	 * Bestellmenge
 	 */
@@ -90,6 +112,83 @@ public class OrderService {
 		this.amount = amount;
 	}
 	
+	public int[][] MatrixMultiplication(){
+		int[][] result = new int[23][4];
+		
+		//TODO Durch Inputvariablen ersetzen
+		int[][] forcast = {
+	            {70, 150, 150, 150},
+	            {190, 150, 150, 150},
+	            {110, 150, 170, 180}
+	            };
+
+		int[][] usage = {
+	            {1, 0, 0},
+	            {0, 1, 0},
+	            {0, 0, 1},
+	            {7, 7, 7},
+	            {4, 4, 4},
+	            {2, 2, 2},
+	            {4, 5, 6},
+	            {3, 3, 3},
+	            {0, 0, 2},
+	            {0, 0, 72},
+	            {4, 4, 4},
+	            {1, 1, 1},
+	            {1, 1, 1},
+	            {1, 1, 1},
+	            {2, 2, 2},
+	            {1, 1, 1},
+	            {1, 1, 1},
+	            {2, 2, 2},
+	            {1, 1, 1},
+	            {3, 3, 3},
+	            {1, 1, 1},
+	            {1, 1, 1},
+	            {1, 1, 1},
+	            {2, 2, 2},
+	            {2, 0, 0},
+	            {72, 0, 0},
+	            {0, 2, 0},
+	            {0, 72, 0},
+	            {2, 2, 2}
+	            };
+		
+		for(int i = 0; i < result.length; i++){
+			for(int j = 0; j < result[i].length;j++){	
+				int v= 0;
+				for (int n=0; n < forcast.length; n++){
+					v += usage[i][n]*forcast[n][j];
+				}
+				result[i][j]= v;
+			}
+		}
+		return result;
+	}
 	
+	public List<Double> calculateAverageNeeds(){
+		List <Double> average = new ArrayList<Double>();
+		
+		for(int i=0; i<needs.length; i++){
+			double var = 0;
+			for(int j=0; j< needs[i].length; j++){
+				var += needs[i][j];
+			}
+			average.add(var/4);
+		}
+		
+		
+		return average;
+	}
+	
+	//TODO Reichweitensicherung berechnen
+	public List<Integer> calculateCoverage(){
+		List <Integer> coverage = new ArrayList<Integer>();
+		
+		
+		
+		return coverage;
+		
+	}
 	
 }
