e67a0eaac2e253ace7fb3e7b2763a5fe88845455
haeff <haeff@SKYNET01>
2013-01-14 11:15:41 +0100

    new tabs

---
+++ scstool/src/scstool/gui/tab/SettingsTab.java
@@ -0,0 +1,37 @@
+package scstool.gui.tab;
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.MatteBorder;
+
+import scstool.gui.comp.ButtonPane;
+
+public class SettingsTab extends JPanel 
+{
+
+	private static final long serialVersionUID = 1L;
+
+	//Button Panel
+	private ButtonPane bnt_pane;
+	
+	
+	public SettingsTab()
+	{
+		init();
+	}
+	
+	private void init()
+	{
+		
+
+	    
+	    
+	    
+	}
+}
